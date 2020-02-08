c3a5440439aabba857086c0374e491519af33a11
Leon <leon.frese@uzh.ch>
2020-02-08 22:40:58 +0100

    init 7kyu_Is_this_a_triangle.py

---
+++ 7kyu_Is_this_a_triangle.py
@@ -0,0 +1,11 @@
+# link: https://www.codewars.com/kata/56606694ec01347ce800001b
+
+def is_triangle(a,b,c):
+
+    values_list  = [a, b, c]
+    values_list.sort()
+
+    if (values_list[0] + values_list[1]) > values_list[2]:
+        return(True)
+
+    return(False)
