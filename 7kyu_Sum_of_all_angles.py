d1e870b3c773453e4fa250086403762cf1c54778
Leon <leon.frese@uzh.ch>
2020-02-09 21:25:39 +0100

    7kyu_Sum_of_all_angles.py

---
+++ 7kyu_Sum_of_all_angles.py
@@ -0,0 +1,9 @@
+# link: https://www.codewars.com/kata/5a03b3f6a1c9040084001765
+
+def angle(n):
+    
+    # calculate the interior angle
+    res = (n-2)*180
+    
+    # return the interior angle
+    return(res)
